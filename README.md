404470d7a9feda239fbedd2cf8d88bdf4dbb66a4
tarpetos <hevlichtaras@gmail.com>
2023-01-31 16:14:00 +0200

    Add button holder and requirements.txt. Update autoclicker and README.md

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Auto Clicker 
-A simple clicker that allows you to click in the selected window 1 time in a certain period of time.  
-This time period is set when the application is launched and persists until the application is restarted.  
+A simple console clicker that allows you to make a global click in your operating system 1 time in a certain period of time.  
+This time period is set when the application is launched and persists until the application is stopped.  
+Also there is a function that allows you to hold one button (except `CTRL`). You have to enter the value of this button like a string.  
+If you enter more than 1 key value (or `CTRL`), the key hold will not work.
 
-To stop the clicker, you need to hold the `CTRL` key until the program completes its execution.
+To stop the clicker, you need to press the `CTRL` key until the program returns to the main menu.  
+To fully stop the program, you need to enter the `3` key in the main menu and press `Enter` or close your console.
--- main.py
+++ main.py
@@ -1,33 +1,60 @@
+import os
+import platform
 import pyautogui
 import time
-import keyboard
+from pynput import keyboard
 
 
-def time_beetwen_clicks() -> float:
-    error_message = 'Invalid input! Must be positive float number.\n'
+def os_console_clear():
+    user_os = platform.uname().system
 
+    if user_os == 'Linux' or user_os == 'MacOS':
+        os.system('clear')
+    elif user_os == 'Windows':
+        os.system('clr')
+
+
+def invalid_autoclicker_input():
+    os_console_clear()
+    print('Invalid input! Must be positive float or integer number.\n')
+    input('Press Enter to continue...\n')
+    os_console_clear()
+
+
+def time_between_clicks() -> float:
     while True:
         try:
-            user_input_str = input('Enter time delay beetwen clicks: ')
+            user_input_str = input('Enter time delay between clicks: ')
             user_input = user_input_str.replace(',', '.')
 
             duration = float(user_input)
 
             if duration <= 0:
-                print(error_message)
+                invalid_autoclicker_input()
                 continue
             else:
                 return duration
         except ValueError:
-            print(error_message)
+            invalid_autoclicker_input()
             continue
 
 
-def click_cycle():
-    duration = time_beetwen_clicks()
+def button_to_hold():
+    user_button = input('Enter the key value that will be held: ')
+    return user_button
+
+
+def execute_program(user_input):
+    os_console_clear()
+
+    if user_input == 1:
+        duration = time_between_clicks()
+    else:
+        user_button = button_to_hold()
 
     print(
-        '\nApplication will start after 10 seconds. \nOpen the window where you want to automate the clicking process.'
+        '\nApplication will start after 10 seconds. '
+        '\nOpen the window where you want to automate the clicking or holding process.'
     )
 
     for second in range(10):
@@ -35,17 +62,61 @@ def click_cycle():
         time_to_wait = 10 - second
         print('Wait', time_to_wait, 's...')
 
-    print('\nClicker was started!!!\nTo exit, hold the "Ctrl" button until the application stops.\n')
+    print(
+        '\nClicker is launched!!!' if user_input == 1 else '\nButton holder is launched!!!',
+        '\nTo stop the program, press the "Ctrl" key until it returns to the main menu.\n'
+    )
 
-    x, y = pyautogui.position()
+    def on_press(key):
+        if key == keyboard.Key.ctrl:
+            print('=' * 10, 'Return to main menu...', '=' * 10, '\n')
+            os_console_clear()
+            listener.stop()
+
+    listener = keyboard.Listener(on_press=on_press)
+    listener.start()
+
+    if user_input == 1:
+        while listener.is_alive():
+            pyautogui.click()
+            pyautogui.PAUSE = duration
+    else:
+        while listener.is_alive():
+            pyautogui.press(user_button, interval=0.01, _pause=False)
+
+
+def menu():
+    print('1. Autoclicker')
+    print('2. Button holder')
+    print('3. Exit')
+    print('Choose an option: ', sep='', end='')
+
+
+def process_user_choice():
+    os_console_clear()
     while True:
-        if keyboard.is_pressed('Ctrl'):
-            print('=' * 10, 'EXITING PROGRAM', '=' * 10)
-            return
+        try:
+            menu()
+            user_input = int(input())
 
-        pyautogui.click(x, y)
-        pyautogui.PAUSE = duration
+            if user_input == 1 or user_input == 2:
+                execute_program(user_input)
+            elif user_input == 3:
+                return
+            else:
+                invalid_input()
+                continue
+        except ValueError:
+            invalid_input()
+            continue
+
+
+def invalid_input():
+    os_console_clear()
+    print('Invalid input! Possible options: 1, 2, 3. Try again.\n')
+    input('Press Enter to continue...\n')
+    os_console_clear()
 
 
 if __name__ == '__main__':
-    click_cycle()
+    process_user_choice()
